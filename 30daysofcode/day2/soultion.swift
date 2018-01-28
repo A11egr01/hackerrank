306c79523994221987de17bbfa4323848d723cef
Alexander Sosnov <AlexanderSosnov@AMP.local>
2018-01-27 16:22:48 -0800

    d2

---
+++ 30daysofcode/day2/soultion.swift
@@ -0,0 +1,19 @@
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+
+// Declare second integer, double, and String variables.
+
+var line1: Int
+var line2: Double
+var line3: String
+
+// Read and save an integer, double, and String to your variables.
+line1 = Int(readLine()!)!
+line2 = Double(readLine()!)!
+line3 = readLine()!
+
+// Print the sum
+print(i + line1)
+print(d + line2)
+print(s + line3)
