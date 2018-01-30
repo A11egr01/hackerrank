19239c2f51554d05189238dbcb8d84638f2f48a5
Alexander Sosnov <asosnov@student.42.us.org>
2018-01-29 19:52:03 -0800

    day3: intro to conditional statements

--- 30daysofcode/day3/solution.swift
+++ 30daysofcode/day3/solution.swift
@@ -1,1 +1,16 @@
-!
+
+import Foundation
+
+let number = Int(readLine()!)!
+
+if number % 2 != 0{
+    print("Weird")
+} else {
+    if number < 5 {
+        print ("Not Weird")
+    } else if number > 20 {
+        print("Not Weird")
+    } else {
+        print("Weird")
+    }
+}
