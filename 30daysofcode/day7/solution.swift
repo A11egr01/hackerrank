aefac58c1d8dd099dc7f6b37bcedd90a64a49a03
Alexander Sosnov <asosnov@student.42.us.org>
2018-02-06 22:15:19 -0800

    day7

---
+++ 30daysofcode/day7/solution.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+// number of elements
+let n = Int(readLine()!)!
+
+// read array
+let arr = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+
+// iterate over the array in reverse order and print the elements separated by space
+
+for str in arr.reversed() {
+    print(str, terminator: " ")
+}
